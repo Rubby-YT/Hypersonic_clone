b7af5fc66344414e9c1465edeb8b23ade994116a
Rubby-YT <ethanmeador1@gmail.com>
2023-11-19 01:07:02 -0500

    renamed Author

--- ModHelperData.cs
+++ ModHelperData.cs
@@ -3,11 +3,12 @@ namespace Hypersonic_clone;
 public static class ModHelperData
 {
     public const string WorksOnVersion = "39.0";
-    public const string Version = "1.0.0";
+    public const string Version = "1.0.3";
     public const string Name = "Hypersonic clone";
 
-    public const string Description = "An empty mod";
+    public const string Description = "just a test";
 
     public const string RepoOwner = "Rubby-YT"; // TODO add your github username hero, also in the download url in README.md
     public const string RepoName = "Hypersonic_clone"; // TODO add your repo name here, also in the download url in README.md
+    public const string Author = "Rubby";
 }
